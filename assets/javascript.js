6aa5ea9e5e42df178e9477fe2e7a054b41b5fcd5
denis salvino <denis.salvino23@gmail.com>
2021-03-16 21:03:21 -0500

    wheres the javascript denis?....

---
+++ assets/javascript.js
@@ -0,0 +1,50 @@
+var questions = [
+    {
+        question: "what does HTML stand for?",
+        answers: {
+            a: "Home Tool Markup Language",
+            b: "Hyper Text Markup Language",
+            c: "Hyperlinks and Text Markup Language"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who is making the Web Standards?",
+        answers: {
+            a: "Google",
+            b: "Microsoft",
+            c: "The World Wide Web Consortium",
+            d: "Mozilla"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Choose the correct HTML element for the largest heading:",
+        answers: {
+            a: "<h6>",
+            b: "<head>",
+            c: "<h1>",
+            d: "<heading>"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the correct HTML element for inserting a line Break?",
+        answers: {
+            a: "<br>",
+            b: "<break>",
+            c: "<lb>"
+    },
+        correctAnswer: "a"
+    },
+    {
+        question: "Choose the correct HTML element to define emhpasized text",
+        answers: {
+            a: "<em>",
+            b: "<italic>",
+            c: "<i>"
+        },
+        correctAnswer: "c"
+    }
+];
+
